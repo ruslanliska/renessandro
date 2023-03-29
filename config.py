1a905ed75fc00ad7b5682922bca54c8c73101878
Ruslan Liska <ruslanliska@Ruslans-MacBook-Air.local>
2023-03-29 20:43:05 +0300

    Created bot for downloading images from MJ request result and updated requirements

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ load_dotenv()
 
 DISCORD_EMAIL = os.getenv("DISCORD_EMAIL")
 DISCORD_PASSWORD = os.getenv("DISCORD_PASSWORD")
+OPENAI_KEY = os.getenv("OPENAI_KEY")
+DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
