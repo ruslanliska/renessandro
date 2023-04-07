3e104c065499b0bdcfeff41a1baeb58ba20d6316
Ruslan Liska <ruslanliska@Ruslans-MacBook-Air.local>
2023-04-07 15:29:37 +0300

    Created form for custom request with provided data for creative

--- flask_application/forms.py
+++ flask_application/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 
 
-class MyForm(FlaskForm):
+class CustomRequestForm(FlaskForm):
     # name = StringField("Request Text", validators=[DataRequired()])
-    name = StringField("Request Text")
+    subject_1 = StringField("Subject 1", validators=[DataRequired()])
+    subject_2 = StringField("Subject 2")
+    place = StringField("Place", validators=[DataRequired()])
+    action = StringField("Action", validators=[DataRequired()])
+    quantity = IntegerField("How much creatives do you want? (Divided by 4)")
     submit = SubmitField("Submit")
