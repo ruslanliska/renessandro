ded7fc857c37851c891af9de4694334d8a3f1aa5
ruslanliska56 <ruslan@talentpath.co>
2023-09-04 15:17:26 +0300

    Created functionality to read json file

--- openai_api/picture_data.py
+++ openai_api/picture_data.py
@@ -1,8 +1,24 @@
-picture = {
-  "subject_1": ["a billionaire", "a fake billionaire", "werwolf", "a man dragon", "drug dealer", "bad boy", "twins", "prince", "cowboy", "professor"],
-  "subject_1_description": ["in good shape", "muscular", "sporty and attractive", "cute", "old", "very young"],
-  "subject_2": ["surrogate mother", "assistant female", "wife", "mistress", "princes", "poor young women",  "student female"],
-  "subject_2_description": ["with kiss marks on the neck", "in wedding Dress", "with a pregnancy test in her hand", "pregnant with big opened belly", "very big waist"],
-  "action": ["he kisses her", "he hugs her", "he holds her in his arms", "he push her against the wall", "he marry her", "he hugs her from behind", "one night stand"],
-  "place": ["a bedroom with a large bed", "luxury penthouse", "luxury villa", "sports car", "island", "yacht", "altar for marriage", "office", "dark forest", "dungeon"]
-}
+import json
+
+
+def read_json_file(json_file_path: str):
+    """
+    Read a JSON file and parse its content into a Python dictionary.
+
+    Args:
+        json_file_path (str): The path to the JSON file to be read.
+
+    Returns:
+        dict: A Python dictionary containing the parsed JSON data.
+
+    Example Usage:
+        json_data = read_json_file('data.json')
+    """
+    # Open and read the JSON file
+    with open(json_file_path, 'r') as json_file:
+        # Parse the JSON content into a Python dictionary
+        picture_data = json.load(json_file)
+    return picture_data
+
+
+picture = read_json_file('picture.json')
