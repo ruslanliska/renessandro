ae93f891561a32f758f709079da32405d6f335f6
ruslanliska56 <ruslan@talentpath.co>
2023-09-13 13:15:44 +0300

    Added driver quit

--- openai_api/mj_request.py
+++ openai_api/mj_request.py
@@ -31,6 +31,7 @@ class MJConnection:
     def close_selenium_driver(self) -> None:
         print('Driver closing')
         self.driver.close()
+        self.driver.quit()
 
     def mj_request(self, request_text: str) -> None:
         mj_start_text = f"/imagine "
